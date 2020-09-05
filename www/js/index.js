aaeb07afd506828f90a368b229fd41679d00e429
Jarana <powerplay.sl.info@gmail.com>
2020-09-05 17:35:29 +0200

    Añade comentarios

--- www/js/index.js
+++ www/js/index.js
@@ -1,44 +1,17 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+/* JS asociado al archivo index.html cuyo estilo está en el archivo index.css */
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+/* Añade una función como respuesta a un evento determinado    */
 document.addEventListener('deviceready', onDeviceReady, false);
 
-document.body.addEventListener("click", cambiarPagina, false);
+/* Añade una función como respuesta a un evento sobre un elemento del html determinado */
+document.body.addEventListener("click", cambiarPagina);
 
+/* Función que añade una clase al elemento devicerady para cambiar su aspecto */
 function onDeviceReady() {
-   // Cordova is now initialized. Have fun!
-
-   console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
    document.getElementById('deviceready').classList.add('ready');
 }
 
-function cambiarPagina(){
+/* Función que muestra la ventana de iniciar sesión   */
+function cambiarPagina() {
    location.href = "inicio.html";
 }
-
-/*
-function validClick()
-   {
-      //valid.performClick();
-      //document.getElementById("inicioButton").textContent="OK";
-   }
-
-*/
--- www/js/inicio.js
+++ www/js/inicio.js
@@ -1,3 +1,12 @@
+/*  JS asociado al archivo inicio.html cuyo estilo está en el archivo index.css */
+
+/*  Constantes  */
+const ERROR_USUARIO = "Usuario o contraseña incorrecto";
+const ERROR_CONTRASEÑA = "Usuario no registrado";
+const DELAY_MS = 10000;
+
+/*  Array que contiene los usuarios y contraseñas registrados (No definitivo: esta información debe
+    estar en la base de datos)  */
 const usuarios = [{
         username: "pepe",
         pass: "pep1234"
@@ -12,6 +21,7 @@ const usuarios = [{
     }
 ];
 
+/*   Añade una función como respuesta a un evento sobre un elemento del html determinado  */
 document.getElementById("enlaceBotonRegistrase").addEventListener("click", irARegistrarse);
 document.getElementById("enlaceBotonRegistrase").addEventListener("pointerdown", botonPulsado);
 document.getElementById("enlaceBotonRegistrase").addEventListener("pointerleave", botonSoltado);
@@ -22,46 +32,62 @@ document.getElementById("instagramBoton").addEventListener("click", instagramIni
 document.getElementById("instagramBoton").addEventListener("pointerdown", botonPulsado);
 document.getElementById("instagramBoton").addEventListener("pointerleave", botonSoltado);
 
-
+/*  Función que muestra la ventana de resgistro */
 function irARegistrarse() {
     location.href = "registro.html";
 }
 
+/*  Función que comprueba las credenciales de inicio de sesión (No tiene en cuenta la opción
+    de mantener sesión iniciada y/o recordar credenciales. Hay que añadirlo)    */
 function iniciarSesion() {
-    const userBuscado = comprobarUsuaio(document.getElementById("name").value);
-    if(userBuscado){
-        if(comprobarContraseña(userBuscado,document.getElementById("passwd").value))
+    const usuarioBuscado = comprobarUsuaio(document.getElementById("name").value);
+    if (usuarioBuscado) {
+        if (comprobarContraseña(usuarioBuscado, document.getElementById("passwd").value))
             location.href = "principal.html";
         else
-            mostarMensaje("Usuario o contraseña incorrecto");
-    }else
-        mostarMensaje("Usuario no registrado");
+            mostarMensaje(ERROR_USUARIO);
+    } else
+        mostarMensaje(ERROR_CONTRASEÑA);
 }
 
+/*  Función que debe cambiar a la ventana de iniciar sesión con Instagram (NO IMPLEMENTADA) */
 function instagramIniciarSesion() {
 
 }
 
+/*  Función que al ser pulsado un elemento le añade una clase para resaltarlo   */
 function botonPulsado(event) {
-    const botonPulsado = event.currentTarget;
+    const botonPulsado = event.currentTarget; //Introduce en la variable botonPulsado el Id del elemento que ha sido pulsado
     botonPulsado.classList.add("pulsacion");
 }
 
+/*  Función que al ser soltado un elemento le quita la clase para resaltarlo    */
 function botonSoltado(event) {
-    const botonSoltado = event.currentTarget;
+    const botonSoltado = event.currentTarget; //Introduce en la variable botonSoltado el Id del elemento que ha sido soltado
     botonSoltado.classList.remove("pulsacion");
 }
 
-function comprobarUsuaio(usuario){
-    return usuarios.find((user)=>user.username == usuario);
+/*  Función que comprueba si el usuario recibido está registrado y devuelve su posición en
+    el array si lo ha encontrado o null si no lo ha hecho (Hay que modificarla para que
+    compruebe el usuario en la base de datos)   */
+function comprobarUsuaio(usuario) {
+    return usuarios.find((user) => user.username == usuario); //.find() Le pasa todos los elementos del array usuarios al la función sin nombre en el parámetro user para que compruebe si coincide con el elemento buscado
 }
 
-function comprobarContraseña(usuario, contraseña){
+/*  Función que comprueba si la contraseña recibida corresponde al usuario recibido y devuelve
+    true si la contraseña es correcta o false si no lo es (Hay que modificarla para que
+    compruebe la contraseña en la base de datos)    */
+function comprobarContraseña(usuario, contraseña) {
     return contraseña == usuario.pass;
 }
 
-function mostarMensaje(texto){
-    document.getElementById("mensaje").innerHTML = texto;
-    mensaje.classList.add("visible");
-    setTimeout(()=>{mensaje.classList.remove("visible")},10000);
+/*  Función que muestra durante 10s el mensaje recibido como alerta de error al iniciar sesión
+    (Cuando se llama a esta función antes de los 10s el mensaje cambia pero desaparece al completarse
+    los 10s de la primera ejecución)  */
+function mostarMensaje(texto) {
+    document.getElementById("mensaje").textContent = texto; //Modifica el texto del elemento mensaje
+    mensaje.classList.add("visible"); //Añade una clase al elemento mensaje para que sea visible
+    setTimeout(() => { //setTimeout ejecuta la función del primer parámetro cuando ha pasado el tiempo indicado en el segundo parámetro en ms
+        mensaje.classList.remove("visible") //La función sin nombre y con return implicito quita la clase al elemento mensaje para que vuelva a ser invisible
+    }, DELAY_MS);
 }
