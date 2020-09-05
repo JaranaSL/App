c1ef494aba94cc0a0349ad467ecddae2cb06033c
Jarana <powerplay.sl.info@gmail.com>
2020-09-05 16:05:41 +0200

    Añade la comprobación de las credenciales

--- www/js/inicio.js
+++ www/js/inicio.js
@@ -1,3 +1,17 @@
+const usuarios = [{
+        username: "pepe",
+        pass: "pep1234"
+    },
+    {
+        username: "paco",
+        pass: "pac1234"
+    },
+    {
+        username: "juan",
+        pass: "jua1234"
+    }
+];
+
 document.getElementById("enlaceBotonRegistrase").addEventListener("click", irARegistrarse);
 document.getElementById("enlaceBotonRegistrase").addEventListener("pointerdown", botonPulsado);
 document.getElementById("enlaceBotonRegistrase").addEventListener("pointerleave", botonSoltado);
@@ -14,7 +28,14 @@ function irARegistrarse() {
 }
 
 function iniciarSesion() {
-    location.href = "principal.html";
+    const userBuscado = comprobarUsuaio(document.getElementById("name").value);
+    if(userBuscado){
+        if(comprobarContraseña(userBuscado,document.getElementById("passwd").value))
+            location.href = "principal.html";
+        else
+            mostarMensaje("Usuario o contraseña incorrecto");
+    }else
+        mostarMensaje("Usuario no registrado");
 }
 
 function instagramIniciarSesion() {
@@ -29,4 +50,18 @@ function botonPulsado(event) {
 function botonSoltado(event) {
     const botonSoltado = event.currentTarget;
     botonSoltado.classList.remove("pulsacion");
+}
+
+function comprobarUsuaio(usuario){
+    return usuarios.find((user)=>user.username == usuario);
+}
+
+function comprobarContraseña(usuario, contraseña){
+    return contraseña == usuario.pass;
+}
+
+function mostarMensaje(texto){
+    document.getElementById("mensaje").innerHTML = texto;
+    mensaje.classList.add("visible");
+    setTimeout(()=>{mensaje.classList.remove("visible")},10000);
 }
